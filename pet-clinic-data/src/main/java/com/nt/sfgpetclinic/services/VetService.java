db176aea1539eafdcc010a3477c50cc8c72d065f
nt <nataliia.tolmachova@gmail.com>
2019-01-08 17:43:58 +0100

    Implement Interfaces for Services. closes #5

---
+++ pet-clinic-data/src/main/java/com/nt/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.nt.sfgpetclinic.services;
+
+import com.nt.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
